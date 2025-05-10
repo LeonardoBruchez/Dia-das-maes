ff57040c873e3365a85195267f0cb959554b225a
Leonardo Bruchez <bruchezleonardo@gmail.com>
2025-05-10 20:14:13 -0300

    adicionando mais fotos

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ function createHeart() {
   // Criar um coração novo a cada 500ms
   setInterval(createHeart, 500);
   
+  function mostrarMensagem(estado) {
+    const mensagem = document.getElementById("mensagem");
+    let texto = "";
+  
+    switch (estado) {
+      case "feliz":
+        texto = "Que bom que você está feliz! Você merece todos os sorrisos do mundo. 😄";
+        break;
+      case "triste":
+        texto = "Ei, mamãe… tudo vai ficar bem. Respira fundo. Estou com você sempre. 💖";
+        break;
+      case "carinho":
+        texto = "Você é a melhor mãe do universo! Receba todo meu amor e um abraço apertado. 🤗";
+        break;
+      case "forca":
+        texto = "Você é forte, guerreira e linda. Já venceu tanto! Nunca se esqueça disso. 💪💝";
+        break;
+      default:
+        texto = "";
+    }
+  
+    mensagem.textContent = texto;
+  }
+  
